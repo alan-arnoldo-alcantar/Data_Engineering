47df8f6a7a3aa458263295b1272408ffa507a488
alanaag <alan.alcantargz@gmail.com>
2024-06-01 19:45:26 -0600

    Consulta de todos los datos y filtrado

--- fast_api/main.py
+++ fast_api/main.py
@@ -7,6 +7,7 @@ from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
 from config.database import session, engine, base
 from models.movie import Movie as MovieModel # se cambio para no cinfundir con el esquema de la API
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 app.title = "Mi aplicacion con FastAPI"
@@ -79,14 +80,17 @@ def login(user:User):
 
 @app.get('/movies', tags=['movies'], response_model=List[Movie], dependencies=[Depends(JWTBearer())])
 def get_movies() -> List[Movie]:
-    return JSONResponse(status_code=200,content=movies)
+    db = session()
+    result = db.query(MovieModel).all()
+    return JSONResponse(status_code=200,content=jsonable_encoder(result))
 
 @app.get('/movie/{id}', tags=['movies'], response_model=Movie)
 def get_movie(id:int=Path(ge=1,le=2000)) -> Movie:
-    for item in  movies:
-        if item['id'] == id:
-            return JSONResponse(content=item)
-    return JSONResponse(status_code=200,content=[])
+    db = session()
+    result = db.query(MovieModel).filter(MovieModel.id == id).first()
+    if not result:
+        return JSONResponse(status_code=404, content={'message':'No encontrado'})
+    return JSONResponse(status_code=200,content=jsonable_encoder(result))
 
 @app.get('/movies/', tags=['movies'], response_model=List[Movie])
 def get_movies_by_category(category:str=Query(min_length=2,max_length=15)) -> List[Movie]:
